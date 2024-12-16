7a6e3122ff8b70ec7fe4217e4fead7cc52e0ab05
raffiahmadsp <raphysp65@gmail.com>
2024-12-16 10:54:44 +0700

    model company peofile

--- app/Models/CompanyProfile.php
+++ app/Models/CompanyProfile.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class CompanyProfile extends Model
 {
-    //
+    protected $table = 'company_profile';
+    protected $primaryKey = 'id_company';
+
+    protected $fillable = [
+        'company_name',
+        'logo',
+        'visi_company',
+        'misi_company',
+        'history',
+        'addres',
+        'phone_company',
+        'email_company'
+    ];
 }
